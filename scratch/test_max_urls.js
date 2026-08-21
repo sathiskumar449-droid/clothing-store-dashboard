8a6591bb3b6675719471bcd1a1d29f96a0a0f384
Sathish Kumar <sathiskumar449@gmail.com>
2026-08-21 07:03:27 +0530

    fix: max shorts url mapping

---
+++ scratch/test_max_urls.js
@@ -0,0 +1,9 @@
+import { getCategoryUrl } from '../lib/categoryUrls.js';
+
+console.log('--- URL Tests ---');
+console.log('max:', getCategoryUrl('max'));
+console.log('max shorts:', getCategoryUrl('max shorts'));
+console.log('Max Shorts:', getCategoryUrl('Max Shorts'));
+console.log('max short:', getCategoryUrl('max short'));
+console.log('Shorts:', getCategoryUrl('Shorts'));
+console.log('Imported Shorts:', getCategoryUrl('Imported Shorts'));
